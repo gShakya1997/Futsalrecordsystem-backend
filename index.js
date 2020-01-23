49b6ed65e28f0e33178c79872e379d3c717f2fb7
gunjan <gunjan.shakya3@gmail.com>
2020-01-23 09:46:12 +0545

    database connection

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const morgan = require('morgan');
 const cors = require('cors');
+const mongoose = require("mongoose");
+const dotenv = require('dotenv').config();
 
 const app = express();
 app.use(morgan("tiny"));
 app.use(express.json());
-app.options("*",cors());
+app.options("*", cors());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(__dirname + "/public"));
+
+//database connection
+mongoose.connect(process.env.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then((db) => {
+    console.log("Successfully connected to MongoDB server");
+}, (err) => console.log(err));
+
+app.listen(process.env.PORT,()=>{
+    console.log(`App is running at localhost:${process.env.PORT}`);
+});
